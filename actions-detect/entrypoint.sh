7b5ed405c472fe8119d07c1ee7b5fc889de3adaa
Gautam Baghel <gautamb@gautamb-mac.local>
2019-04-09 17:42:32 -0400

    omg, work goddamit

--- actions-detect/entrypoint.sh
+++ actions-detect/entrypoint.sh
@@ -1,10 +1,9 @@
-#!/bin/sh
-
-bash <(curl -s https://blackducksoftware.github.io/hub-detect/hub-detect.sh) \
+#!/bin/bash
+bash <(curl -s https://detect.synopsys.com/detect.sh) \
 --blackduck.url="https://bizdevhub.blackducksoftware.com" \
---blackduck.api.token="MDVlYWEyODQtMzc5NS00NzVkLWJhN2MtN2M4YWY3ZmUwMjJiOjRmNjc0OWEyLWFiZjUtNDgwNS05ZjBjLTllNzJmNjVmYmNhNQ==" \
---detect.project.name="DUCKY-ACTIONS" \
---detect.project.version.name="1" \
+--blackduck.api.token="NWU3NzM4MzQtMWU3Yi00MjVkLThkZTMtNTVlNzQyY2Q0ODFkOjdkOWM5NGJiLTRhZDUtNDk3Yy04NDdlLWMyNmFmMDBkYTg4ZA==" \
+--detect.project.name="JenkinsDucky" \
+--detect.project.version.name="${BUILD_TAG}" \
 --detect.policy.check.fail.on.severities=ALL \
 --detect.risk.report.pdf=true \
 --blackduck.trust.cert=true \
