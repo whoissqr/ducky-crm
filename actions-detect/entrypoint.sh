578c909c6773388a08339cc21a6845d886fdf251
Gautam Baghel <gautamb@gautamb-mac.internal.synopsys.com>
2019-04-10 10:00:10 -0400

    work goddammit

--- actions-detect/entrypoint.sh
+++ actions-detect/entrypoint.sh
@@ -4,7 +4,6 @@ bash <(curl -s https://detect.synopsys.com/detect.sh) \
 --blackduck.api.token="NWU3NzM4MzQtMWU3Yi00MjVkLThkZTMtNTVlNzQyY2Q0ODFkOjdkOWM5NGJiLTRhZDUtNDk3Yy04NDdlLWMyNmFmMDBkYTg4ZA==" \
 --detect.project.name="DuckyCrmActions" \
 --detect.project.version.name="1.0.2-gautam-actions" \
---detect.policy.check.fail.on.severities=ALL \
 --detect.risk.report.pdf=true \
 --blackduck.trust.cert=true \
 --detect.api.timeout=9000000
