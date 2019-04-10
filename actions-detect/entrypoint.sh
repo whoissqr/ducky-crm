5b39cf3b52faa5e7b20468b4b6a0f60403035cf6
Gautam Baghel <gautamb@gautamb-mac.internal.synopsys.com>
2019-04-10 09:17:05 -0400

    okay, i'm officially embarrased now

--- actions-detect/entrypoint.sh
+++ actions-detect/entrypoint.sh
@@ -3,7 +3,6 @@ bash <(curl -s https://detect.synopsys.com/detect.sh) \
 --blackduck.url="https://bizdevhub.blackducksoftware.com" \
 --blackduck.api.token="NWU3NzM4MzQtMWU3Yi00MjVkLThkZTMtNTVlNzQyY2Q0ODFkOjdkOWM5NGJiLTRhZDUtNDk3Yy04NDdlLWMyNmFmMDBkYTg4ZA==" \
 --detect.project.name="JenkinsDucky" \
---detect.project.version.name="${BUILD_TAG}" \
 --detect.policy.check.fail.on.severities=ALL \
 --detect.risk.report.pdf=true \
 --blackduck.trust.cert=true \
