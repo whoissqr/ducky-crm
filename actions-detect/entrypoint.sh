9aff9af682f058a116f2a16a48426080fbe5e859
Gautam Baghel <gautamb@gautamb-mac.internal.synopsys.com>
2019-04-10 09:39:58 -0400

    need to find something faster

--- actions-detect/entrypoint.sh
+++ actions-detect/entrypoint.sh
@@ -2,8 +2,8 @@
 bash <(curl -s https://detect.synopsys.com/detect.sh) \
 --blackduck.url="https://bizdevhub.blackducksoftware.com" \
 --blackduck.api.token="NWU3NzM4MzQtMWU3Yi00MjVkLThkZTMtNTVlNzQyY2Q0ODFkOjdkOWM5NGJiLTRhZDUtNDk3Yy04NDdlLWMyNmFmMDBkYTg4ZA==" \
---detect.project.name="JenkinsDucky" \
+--detect.project.name="DuckyCrmActions" \
+--detect.project.version.name="1.0.2-gautam-actions" \
 --detect.policy.check.fail.on.severities=ALL \
 --detect.risk.report.pdf=true \
 --blackduck.trust.cert=true \
---detect.api.timeout=900000
