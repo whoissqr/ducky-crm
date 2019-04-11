4f9ba26122ac0e099c0cc63a31a549965e4ad586
Gautam Baghel <gautamb@gautamb-mac.internal.synopsys.com>
2019-04-11 17:05:19 -0400

    detect should work

--- actions-detect/entrypoint.sh
+++ actions-detect/entrypoint.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 find ${HOME}
+find ${GITHUB_WORKSPACE}
 
 # bash <(curl -s https://detect.synopsys.com/detect.sh) \
 # --blackduck.url="$BLACKDUCK_URL" \
--- maven-cli/entrypoint.sh
+++ maven-cli/entrypoint.sh
@@ -5,3 +5,4 @@ set -e
 sh -c "mvn $*"
 
 find ${HOME}
+find ${GITHUB_WORKSPACE}
