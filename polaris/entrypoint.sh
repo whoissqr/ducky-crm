c8e031685f2c469ea19262c4f1d10e9add32bae0
Gautam <>
2019-10-04 10:35:45 -0400

    fresh copy

--- polaris/entrypoint.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-mkdir /.swip && wget https://polaris.synopsys.com/api/tools/swip_cli-linux64.zip -O /tmp/swip_cli-linux64.zip \
-&& unzip /tmp/swip_cli-linux64.zip -d /tmp/swip_cli-linux64 \
-&& rm /tmp/swip_cli-linux64.zip && cp /tmp/swip_cli-linux64/**/bin/* /usr/bin
-
-swip_cli analyze -w  
